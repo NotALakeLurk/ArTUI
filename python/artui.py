74bc73f7c6af58e6c8d5ccdefcab27752f17b4ed
John D. Reed <theredcoat787@gmail.com>
2023-07-12 19:39:04 -0400

    Custom pad size

--- python/artui.py
+++ python/artui.py
@@ -9,6 +9,7 @@
 """
 
 import curses
+from curses.textpad import Textbox
 
 # Cursor movement keys consts
 MOV_LEFT = ord("h")
@@ -17,63 +18,97 @@ MOV_UP = ord("k")
 MOV_RIGHT = ord("l")
 
 stdscr = None
+scr_maxyx = None # Holds the results of stdscr.getmaxyx()
+pad = None
+pad_maxyx = None # Holds the results of pad.getmaxyx()
+viewport = [0,0, None,None] # A list holding the top left and bottom right corners of the viewed portion of pad
 
-"""
-def get_string(win, y, x, echo=False):
-    win.move(y,x)
-    curses.echo()
-    
-    string = ""
-"""
+def input_T(T, y, x, n=255):
+    """Get input of type T from the user"""
+    editwin = curses.newwin(1,n, y,x)
+    while True:
+        box = Textbox(editwin)
+        box.edit()
+        try:
+            return T(box.gather())
+        except ValueError:
+            editwin.addstr(0,0, f"Enter a(n) {T}")
+            editwin.refresh()
+            editwin.getch()
+            editwin.clear()
+
 
 def init_pad():
-    stdscr.addstr(0,0, "Input desired height and width of drawing")
-    
-    curses.echo()
-    stdscr.addstr(1,0, "Height: ")
-    height = stdscr.getstr(1,8)
-    stdscr.addstr(2,0, "Width: ")
-    width = stdscr.getstr(2,7)
-    curses.noecho()
-    
-    global pad
-    pad = curses.newpad(int(height), int(width))
-
-def init():
     stdscr.clear()
-    init_pad() # Create a new pad of desired size
+    stdscr.addstr(0,0, "Input desired height and width of drawing; your screen is {stdscr.getmaxyx()}")
+    
+    stdscr.addstr(1,0, "Height: ")
+    stdscr.refresh()
+    height = input_T(int, 1,8)+1 # Add 1 to accomodate border (and future status bars)
+    stdscr.addstr(2,0, "Width: ")
+    stdscr.refresh()
+    width = input_T(int, 2,7)+1 # Same
 
-def main(initscr):
+    global scr_maxyx # Set the viewport size
+    if height < scr_maxyx[0]:
+        viewport[2] = height
+    else: viewport[2] = scr_maxyx
+    if width < scr_maxyx[1]:
+        viewport[3] = width
+    else: viewport[3] = scr_maxyx
+
+    global pad
+    pad = curses.newpad(height, width)
+    global pad_maxyx # NOTE: This may need to be a list, as it will change with resizing when that's implemented
+    pad_maxyx = (height, width)
+    pad.border()
+    stdscr.clear()
+    stdscr.refresh()
+    refresh_pad()
+
+def init(initscr):
     global stdscr
     stdscr = initscr
     stdscr.clear()
 
-    init()
+    global scr_maxyx # Get the screen size to determine the viewport later
+    scr_maxyx = tuple(map(lambda n: n-1, stdscr.getmaxyx())) # Subtract 1 from the max y and x because the screen likely doesn't fully show them
+
+    stdscr.addstr(0,0, "Press `q` to exit; h,j,k,l to move")
+    stdscr.getch()
+    stdscr.clear()
+
+    init_pad() # Create a new pad of desired size
+    main()
+
+def refresh_pad(y=0, x=0):
+    """Refresh pad based on the viewport"""
+    global pad
+    pad.refresh(*viewport[0:2], y,x, *viewport[2:4])
+
+def main():
     
-    """
-    stdscr.addstr(0,0, "Press `q` to exit")
-    stdscr.addstr(1,0, f"can_change_color: {curses.can_change_color()}")
-    """
     
     # Test the window by waiting for user input and closing
     while True:
-        pos = stdscr.getyx()
-        max_pos = stdscr.getmaxyx()
-        char = stdscr.getch()
+        pos = pad.getyx()
+        max_pos = pad.getmaxyx()
+        char = pad.getch()
 
         # Shortcuts
         if char == ord("q"):
             return
         # Cursor movement
         elif char == MOV_LEFT and pos[1] > 0: # Restrict cursor to the screen
-            stdscr.move(pos[0], pos[1] - 1)
+            pad.move(pos[0], pos[1] - 1)
         elif char == MOV_DOWN and pos[0] < max_pos[0]-1: # max_pos is *just* off the screen @.@
-            stdscr.move(pos[0] + 1, pos[1])
+            pad.move(pos[0] + 1, pos[1])
         elif char == MOV_UP and pos[0] > 0:
-            stdscr.move(pos[0] - 1, pos[1])
+            pad.move(pos[0] - 1, pos[1])
         elif char == MOV_RIGHT and pos[1] < max_pos[1]-1:
-            stdscr.move(pos[0], pos[1] + 1)
+            pad.move(pos[0], pos[1] + 1)
+        refresh_pad()
 
 # Call the curses wrapper to safely start the TUI; restores terminal state, etc.
-curses.wrapper(main)
+curses.wrapper(init)
 
