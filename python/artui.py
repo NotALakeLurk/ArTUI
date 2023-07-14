b87d4cead6b7e66192c3a261dd8b1e3f59466635
John D. Reed <theredcoat787@gmail.com>
2023-07-13 22:44:48 -0400

    Added text-art capability
    I also changed the refresh screen to clear the pad, eventually I want to not clear the existing art, but I don't have the time to implement that currently

--- python/artui.py
+++ python/artui.py
@@ -1,6 +1,7 @@
 """
 |	ArTUI: A TUI drawing program
 |
+|   # I use git now, so, check that instead!
 |	Author:		John D. Reed
 |	Start Date:	2023-06-20
 |	Last Edit: 2023-06-21
@@ -25,6 +26,7 @@ scr_maxyx = None # Holds the results of stdscr.getmaxyx()
 pad = None
 pad_maxyx = None # Holds the results of pad.getmaxyx()
 viewport = [0,0] # A list holding the top left corner of the visible pad area
+paint_mode = False
 
 def input_T(T, y, x, n=255):
     """Get input of type T from the user"""
@@ -44,6 +46,11 @@ def input_T(T, y, x, n=255):
 
 
 def init_pad():
+    global pad
+    if pad is not None:
+        pad.clear()
+        refresh_pad()
+        
     stdscr.addstr(0,0, f"Input desired height and width of drawing; your screen is {stdscr.getmaxyx()}")
     
     stdscr.addstr(1,0, "Height: ")
@@ -53,7 +60,6 @@ def init_pad():
     stdscr.refresh()
     width = input_T(int, 2,7)+2 # Same
 
-    global pad
     pad = curses.newpad(height, width)
     pad.keypad(True)
     global pad_maxyx # NOTE: This may need to be a list, as it will change with resizing when that's implemented
@@ -119,18 +125,32 @@ def mov_cursor(char):
         adjust_viewport(pos)
         refresh_pad()
 
+def handle_control_char(char):
+    """Perform the correct actions for program control characters"""
+    # NOTE: It may eventually be better to have a list of all control characters to check if a string is actually a control character
+    if char == RESIZE: 
+        init_pad()
+    else: mov_cursor(char)
+
+def paint(char):
+    return
+
+def handle_normal_char(char):
+    if paint_mode: paint(char)
+    else:
+        pad.addch(char)
+        mov_cursor(MOV_LEFT) # Addch moves the cursor to the next cell, so we've got to undo that here
+
 def main():
-    pad.addstr(pad_maxyx[0], 0, "Test")
     pad.move(1,1)
     refresh_pad()
     while True:
-        char = curses.keyname(pad.getch())
-        # Shortcuts
-        if char == QUIT:
-            return
-        elif char == RESIZE: 
-            init_pad()
-        else: mov_cursor(char)
+        char = curses.keyname(pad.getch()) # This essentially does pad.getkey(), except better
+        if char == QUIT: return
+        elif len(char) > 1: # Special characters like ^R or KEY_LEFT have bytestrings of 2 or longer
+            handle_control_char(char)
+        else: handle_normal_char(char)
+
 
 # Call the curses wrapper to safely start the TUI; restores terminal state, etc.
 curses.wrapper(init)
